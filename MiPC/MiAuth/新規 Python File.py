64f0733d3080661c633a6ba06e5db0deb52ebd90
sonyakun <90973770+sonyakun@users.noreply.github.com>
2023-06-15 04:51:53 +0900

    お前いらない

--- MiPC/MiAuth/新規 Python File.py
+++
@@ -1,6 +0,0 @@
-import json
-
-with open("example.json", "r", encoding="utf-8") as f:
-    l = json.load(f)
-with open("example.json", "w", encoding="utf-8") as f:
-    json.dump(l, f, ensure_ascii=False, indent=4)
